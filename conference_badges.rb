cbe8bdc575b4048dd424eec86572aea6eb693d29
kyle smith <hirekyle@someotherkyle.com>
2021-02-08 22:57:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  messages = []
+  array.each do |per|
+    msg = badge_maker(per)
+    messages << msg
+  end
+  messages
+end
+
+def assign_rooms(array)
+  room = []
+  array.each_with_index do |speaker, index|
+    ass = "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    room << ass
+  end
+  room
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |msg_1|
+    puts "#{msg_1}"
+  end
+  assign_rooms(attendees).each do |rm|
+    puts "#{rm}"
+  end
+    
+    
+
+
+end
+
+  
+    
